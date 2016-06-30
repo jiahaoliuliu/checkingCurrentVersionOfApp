e9208dd4fe5868ab2949312bc07d8ecc15f9d569
Jiahao Liu <jiahao.liu@dubizzle.com>
2016-06-30 15:11:34 +0400

    Set the app id and version to be checked

--- app/src/main/java/com/jiahaoliuliu/checkingcurrentversionofapp/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/checkingcurrentversionofapp/MainActivity.java
@@ -5,7 +5,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    private static final String APP_ID = "com.worldspotlightapp.android";
+
+    // The current version to check. This should be lower than the production
+    // version so the app will notify about update
+    private static final String CURRENT_VERSION = "1.10";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
