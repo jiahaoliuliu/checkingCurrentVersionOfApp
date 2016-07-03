696a7d5e0a68649c95df16f46515522c62cf0534
Jiahao Liu <jiahao.liu@dubizzle.com>
2016-07-03 12:00:37 +0400

    Trying to use the android market api but it is not working

--- app/src/main/java/com/jiahaoliuliu/checkingcurrentversionofapp/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/checkingcurrentversionofapp/MainActivity.java
@@ -2,9 +2,14 @@ package com.jiahaoliuliu.checkingcurrentversionofapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import com.gc.android.market.api.MarketSession;
+import com.gc.android.market.api.model.Market;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     private static final String APP_ID = "com.worldspotlightapp.android";
 
@@ -22,5 +27,25 @@ public class MainActivity extends AppCompatActivity {
 
         // Link the views
         mSimpleTextView = (TextView) findViewById(R.id.simple_text_view);
+
+        // Check the latest installed versions
+        checkLatestVersion();
+    }
+
+    private void checkLatestVersion() {
+        final MarketSession marketSession = new MarketSession();
+        marketSession.getContext().setAndroidId(APP_ID);
+        String query = "maps";
+        Market.AppsRequest appsRequest = Market.AppsRequest.newBuilder().setQuery(query)
+            .setStartIndex(0).setEntriesCount(10).setWithExtendedInfo(true).build();
+        marketSession.append(appsRequest, new MarketSession.Callback<Market.AppsResponse>() {
+            @Override public void onResult(Market.ResponseContext responseContext,
+                Market.AppsResponse appsResponse) {
+                    // Log
+                Log.v(TAG, "The response context is " + responseContext + ", and the app response is " +
+                    appsResponse);
+                //marketSession.flush();
+            }
+        });
     }
 }
