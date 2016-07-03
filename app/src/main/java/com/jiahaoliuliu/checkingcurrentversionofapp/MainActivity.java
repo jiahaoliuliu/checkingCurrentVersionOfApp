63baf8603cb86bbc1345b01f3273b05dceb33b00
Jiahao Liu <jiahao.liu@dubizzle.com>
2016-07-03 15:32:55 +0400

    Using volley for the http request

--- app/src/main/java/com/jiahaoliuliu/checkingcurrentversionofapp/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/checkingcurrentversionofapp/MainActivity.java
@@ -5,12 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import org.jsoup.Jsoup;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private static final String APP_ID = "com.worldspotlightapp.android";
+    private static final String APP_URL = "https://play.google.com/store/apps/details?id=" + APP_ID + "&hl=en";
 
     // The current version to check. This should be lower than the production
     // version so the app will notify about update
@@ -27,44 +34,38 @@ public class MainActivity extends AppCompatActivity {
         mSimpleTextView = (TextView) findViewById(R.id.simple_text_view);
 
         // Check the current version
-        GetVersionCode getVersionCode = new GetVersionCode();
-        getVersionCode.execute();
+        checkCurrentVersion();
     }
 
-    private class GetVersionCode extends AsyncTask<Void, String, String> {
-        @Override protected String doInBackground(Void... voids) {
+    private void checkCurrentVersion() {
+        RequestQueue queue = Volley.newRequestQueue(this);
 
-            String newVersion = null;
-            try {
-                newVersion = Jsoup.connect("https://play.google.com/store/apps/details?id="
-                    + APP_ID
-                    + "&hl=en")
-                    .timeout(30000)
-                    .userAgent(
-                        "Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
-                    .referrer("http://www.google.com")
-                    .get()
-                    .select("div[itemprop=softwareVersion]")
-                    .first()
-                    .ownText();
-                return newVersion;
-            } catch (Exception e) {
-                return newVersion;
-            }
-        }
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, APP_URL,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    //Log.v(TAG, "The response is " + response);
 
-        @Override
-        protected void onPostExecute(String onlineVersion) {
-            super.onPostExecute(onlineVersion);
-            Log.v(TAG, "The current online version is " + onlineVersion);
-            if (onlineVersion != null && !onlineVersion.isEmpty()) {
-                if (Float.valueOf(CURRENT_VERSION) < Float.valueOf(onlineVersion)) {
-                    //show dialog
-                    mSimpleTextView.setText("The online version is " + onlineVersion + ". You got to update the current version");
+                    String newVersion =
+                        Jsoup.parse(response).select("div[itemprop=softwareVersion]")
+                        .first().ownText();
+                    Log.v(TAG, "The new version is " + newVersion);
+
+                    if (Float.valueOf(CURRENT_VERSION) < Float.valueOf(newVersion)) {
+                        //show dialog
+                        mSimpleTextView.setText("The online version is " + newVersion + ". You got to update the current version");
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e(TAG, "Error requesting the web page");
+                    mSimpleTextView.setText("Error on getting the version number");
                 }
             }
-            Log.d(TAG, "Current version " + CURRENT_VERSION + "playstore version " + onlineVersion);
-        }
+        );
+
+        queue.add(stringRequest);
     }
 }
 
